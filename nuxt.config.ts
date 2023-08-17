5858991bfb6d81fc4b22de8a09c16793a4dc8a6e
Azmat Ali <azmat1080@gmail.com>
2023-08-17 13:50:57 +0530

    working push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  devtools: { enabled: false },
-  runtimeConfig:{
-    public:{
-      appId:'',
-      serverApiKey:'',
-      rpId:'',
-      rpName:'',
-      rpProtocol:'',
-      db:''
-    }
-  }
-})
+	modules: ["@nuxtjs/tailwindcss"],
+	devtools: { enabled: false },
+	runtimeConfig: {
+		public: {
+			appId: "application-0-ggxan",
+			serverApiKey:
+				"vEvBuBGRdKQFJdzuzJlncTzT2Ro9kLt1QZB6u4DfnpNGgWgKQFMSOe5XuZTnaV3a",
+			rpId: "localhost",
+			rpName: "abcd",
+			rpProtocol: "http",
+			db: "db1",
+		},
+	},
+});
